03ae218c336eed786e993f34aa496174d67cafd3
Abhijit Suprem <asf2182@columbia.edu>
2017-12-29 18:01:34 -0800

    modified files

--- index.py
+++ index.py
@@ -104,4 +104,4 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
-    url_for('static',filename='us-10m.v1.json')
+    #url_for('static',filename='us-10m.v1.json')
